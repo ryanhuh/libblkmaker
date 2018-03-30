5d497723dd3bac90eb921052ada48d0c74bee0fd
Ryan Huh <ryanhuh@gmail.com>
2018-03-31 01:46:08 +0900

    la add

---
+++ XCodelibblkmaker/cgminer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cgminer
+//
+//  Created by Ryan Huh on 2018. 3. 6..
+//  Copyright © 2018년 Ryan Huh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
